dc7bd24e5ed416fa49e9ab0513d9d8ce134ab1e1
Jonfras <jonas.kreuzh@gmail.com>
2022-09-21 16:09:31 +0200

    finished and interface implemented

---
+++ src/net/htlgkr/krejo/primes/ErastosthenesPrimeSieve.java
@@ -0,0 +1,15 @@
+package net.htlgkr.krejo.primes;
+
+//inspiration von http://public.beuth-hochschule.de/~solymosi/lehre/eratosthenes/Sieb.html
+public class ErastosthenesPrimeSieve implements PrimeSieve {
+    public ErastosthenesPrimeSieve(int p) {
+        int MAX = p;
+    }
+
+    public static void main(String[] args) {
+        int number = 20;
+        ErastosthenesPrimeSieve e = new ErastosthenesPrimeSieve(20);
+        System.out.println(number + " ist eine Primzahl : " +PrimeSieve.isPrime(number));
+        PrimeSieve.printPrimes();
+    }
+}
---
+++ src/net/htlgkr/krejo/primes/PrimeSieve.java
@@ -0,0 +1,46 @@
+package net.htlgkr.krejo.primes;
+
+public interface PrimeSieve {
+    final int Max = 20;//(int)Math.sqrt(max)+2;
+    boolean[] numbers = new boolean[Max];
+
+    int[] primes = new int[Max+1];
+
+    public static boolean isPrime(int p) {
+        int counter = 1;
+        primes[0] = 2;
+        if (p % 2 == 0) return false;
+        for (int prime = 3; prime + 2 < p; prime += 2) {
+            if (!numbers[prime]) {
+                primes[counter] = prime;
+                counter++;
+                for (int i = 2; prime * i < Max; i++) {
+                    numbers[prime * i] = true;
+                }
+            }
+
+        }
+        if (!numbers[p]) return true;
+        else return false;
+    }
+
+    public static void printPrimes() {
+        int counter = 1;
+        primes[0] = 2;
+        for (int i = 2; i < Max; i += 2) {
+            numbers[i] = true;
+        }
+        for (int prime = 3; prime < Max; prime += 2) {
+            if (!numbers[prime]) {
+                primes[counter] = prime;
+                counter++;
+                for (int i = 2; prime * i < Max; i++) {
+                    numbers[prime * i] = true;
+                }
+            }
+        }
+        for (int i = 0; i < primes.length; i++) {
+            System.out.println(primes[i]);
+        }
+    }
+}
