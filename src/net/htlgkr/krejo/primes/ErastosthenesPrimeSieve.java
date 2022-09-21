547033677e552841d7c8f765dbd9a73ecf587121
Jonfras <jonas.kreuzh@gmail.com>
2022-09-21 16:11:46 +0200

    commit 4

--- src/net/htlgkr/krejo/primes/ErastosthenesPrimeSieve.java
+++ src/net/htlgkr/krejo/primes/ErastosthenesPrimeSieve.java
@@ -11,5 +11,6 @@ public class ErastosthenesPrimeSieve implements PrimeSieve {
         ErastosthenesPrimeSieve e = new ErastosthenesPrimeSieve(20);
         System.out.println(number + " ist eine Primzahl : " +PrimeSieve.isPrime(number));
         PrimeSieve.printPrimes();
+        System.out.println("commit 4");
     }
 }
